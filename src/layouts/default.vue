c08d2543d8c4f2807f27d811715c52586f450987
Josue Diaz <jdiaz77711@gmail.com>
2019-12-08 23:30:44 +0000

    initial commit

---
+++ src/layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+	<div class="container">
+		<div id="nav">
+			<router-link to="/">Home</router-link> |
+			<router-link to="/about">About</router-link>
+		</div>
+		<slot></slot>
+	</div>
+</template>
+
+<script>
+	export default {};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,55 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div class="container">
+		<!-- header section -->
+		<header class="header">
+			<!-- logo -->
+			<img class="logo" src="@/assets/images/logo.png" alt="logo image" />
+
+			<!-- search bar -->
+			<form action="#" class="search">
+				<input type="text" class="search__input" placeholder="search hotels" />
+				<button class="search__button">
+					<svg class="search__icon">
+						<use
+							xlink:href="@/assets/images/sprite.svg#icon-magnifying-glass"
+						></use>
+					</svg>
+				</button>
+			</form>
+
+			<!-- right hand side navigation -->
+			<nav class="user-nav">
+				<div class="user-nav__icon-box">
+					<svg class="user-nav__icon">
+						<use xlink:href="@/assets/images/sprite.svg#icon-bookmark"></use>
+					</svg>
+					<span class="user-nav__notification">7</span>
+				</div>
+				<div class="user-nav__icon-box">
+					<svg class="user-nav__icon">
+						<use xlink:href="@/assets/images/sprite.svg#icon-chat"></use>
+					</svg>
+					<span class="user-nav__notification">14</span>
+				</div>
+				<div class="user-nav__user">
+					<img
+						src="@/assets/images/user.jpg"
+						alt="user photo"
+						class="user-nav__user-photo"
+					/>
+					<span class="user-nav__user-name"></span>
+				</div>
+			</nav>
+		</header>
+
+		<!-- main content section -->
+		<div class="content">
+			<nav class="sidebar">sidebar navigation</nav>
+			<main class="hotel-view">hotel view</main>
+		</div>
+	</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+	export default {};
 </script>
